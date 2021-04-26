7f37ec2122b36913cdbfa31136dcb73fa75f7537
Brian <bjroden@uark.edu>
2021-04-25 19:52:48 -0500

    Arrow rotations

--- LaunchPad.java
+++ LaunchPad.java
@@ -1,7 +1,3 @@
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
-
 public class LaunchPad extends MovingObstacle {
     public enum direction {UP, DOWN, LEFT, RIGHT};
     private direction dir;
@@ -9,25 +5,25 @@ public class LaunchPad extends MovingObstacle {
 
     //TODO: rotate image
     LaunchPad(int x, int y, int height, int width, direction dir, int lPow) {
-        super("images/arrow.png", x, y, height, width);
+        super(selectImage(dir), x, y, height, width);
         this.dir = dir; 
         this.launchPower = lPow;
     }
 
     LaunchPad(int x, int y, int height, int width, XorY XorY, double movePower, int loc1, int loc2, direction dir, int lPow) {
-        super("images/arrow.png", x, y, height, width, XorY, movePower, loc1, loc2);
+        super(selectImage(dir), x, y, height, width, XorY, movePower, loc1, loc2);
         this.dir = dir; 
         this.launchPower = lPow;
     }
 
     LaunchPad(int x, int y, int height, int width, double xRatio, double yRatio, double movePower, int x1, int x2, int y1, int y2, direction dir, int lPow) {
-        super("images/arrow.png", x, y, height, width, xRatio, yRatio, movePower, x1, x2, y1, y2);
+        super(selectImage(dir), x, y, height, width, xRatio, yRatio, movePower, x1, x2, y1, y2);
         this.dir = dir; 
         this.launchPower = lPow;
     }
 
     LaunchPad(int x, int y, int height, int width, boolean moving, double xRatio, double yRatio, double movePower, int x1, int x2, int y1, int y2, direction dir, int lPow) {
-        super("images/arrow.png", x, y, height, width, moving, xRatio, yRatio, movePower, x1, x2, y1, y2);
+        super(selectImage(dir), x, y, height, width, moving, xRatio, yRatio, movePower, x1, x2, y1, y2);
         this.dir = dir;
         this.launchPower = lPow;
     }
@@ -38,5 +34,24 @@ public class LaunchPad extends MovingObstacle {
 
     public int getLaunchPower() {
         return launchPower;
+    }
+
+    private static String selectImage(direction dir) {
+        String returnString = null;
+        switch(dir) {
+            case UP:
+                returnString = "images/upArrow.png";
+                break;
+            case RIGHT:
+                returnString = "images/rightArrow.png";
+                break;
+            case LEFT:
+                returnString = "images/leftArrow.png";
+                break;
+            case DOWN:
+                returnString = "images/downArrow.png";
+                break;
+        }
+        return returnString;
     }
 }
